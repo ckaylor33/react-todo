283d5d697c222daf9e450b543613f107bd162986
ckaylor33 <kaylor33@hotmail.co.uk>
2021-02-16 09:49:52 +0000

    todo list setup and initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+function App() {
+  return <h2>To do list setup</h2>
+}
+
+export default App
