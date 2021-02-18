a7c570ae5fe9a9b68db51057165604370f50af7f
ckaylor33 <kaylor33@hotmail.co.uk>
2021-02-18 15:15:07 +0000

    Updates

---
+++ precache-manifest.81ca34bd03c95f7b01b3f29f078faf58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf56b1388c49bef1f52940546a62848b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "8f16181e7ace8d43f89b",
+    "url": "/react-todo/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/react-todo/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f16181e7ace8d43f89b",
+    "url": "/react-todo/static/js/main.9f52f107.chunk.js"
+  },
+  {
+    "revision": "b5e4aa65a4bc673b50b7",
+    "url": "/react-todo/static/js/runtime-main.e7eff083.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ckaylor33/react-todo/precache-manifest.90cfabc27bf54ec8ca0e2a4178725e07.js"
+  "/react-todo/precache-manifest.81ca34bd03c95f7b01b3f29f078faf58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ckaylor33/react-todo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
